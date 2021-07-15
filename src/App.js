0adfb94b2223a410b9fc56a581d33268f73535b1
Nigel Penilla <npenilla23@ufl.edu>
2021-07-15 00:02:14 -0400

    Initialized using create-react-app and added Speechly UI elements

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { SpeechProvider, useSpeechContext } from "@speechly/react-client";
+import {
+  PushToTalkButton,
+  PushToTalkButtonContainer,
+  BigTranscript,
+  BigTranscriptContainer,
+  ErrorPanel,
+} from "@speechly/react-ui";
+const SPEECHLY_API_KEY = process.env.REACT_APP_SPEECHLY_API_KEY;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SpeechProvider appId={SPEECHLY_API_KEY} language="en-US">
+          <h1>Voice Movie Search</h1>
+
+          <BigTranscriptContainer>
+            <BigTranscript />
+          </BigTranscriptContainer>
+
+          <PushToTalkButtonContainer>
+            <PushToTalkButton captureKey=" " />
+            <ErrorPanel />
+          </PushToTalkButtonContainer>
+        </SpeechProvider>
       </header>
     </div>
   );
